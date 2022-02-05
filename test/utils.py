f41fe90414a9578d91bb9d173fe60aae00bb4160
samuels <giladsamuels1@gmail.com>
2022-02-05 16:21:19 +0000

    Use logs and test normally

--- test/utils.py
+++ test/utils.py
@@ -1,5 +1,10 @@
-LOG_PATH = "./test_log.txt"
+import os
+LOG_DIR = "./logs"
+LOG_PATH = LOG_DIR + "/test_log.txt"
 def log(msg):
     print(msg)
+    msg += "\n"
+    if not os.path.isdir(LOG_DIR):
+        os.mkdir(LOG_DIR)
     with open(LOG_PATH, 'a') as f:
         f.write(msg)
