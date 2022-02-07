e525f706d4fde2513a960f60691637d4f8824311
1337samuels <giladsamuels1@gmail.com>
2022-02-07 15:52:53 +0000

    Move HB logic to utils.py

--- our_srcs/utils.py
+++ our_srcs/utils.py
@@ -2,6 +2,15 @@ import os
 import logging
 from our_srcs.consts import *
 import datetime
+import random
+import math
+
+from honeybadgerbft.crypto.threshsig.boldyreva import dealer
+from honeybadgerbft.crypto.threshenc import tpke
+import gevent
+from gevent.event import Event
+from gevent.queue import Queue
+
 
 def setup_logging():
     if not os.path.isdir(LOG_DIR):
@@ -25,5 +34,52 @@ def setup_logging():
     logger.addHandler(ch)
     logger.addHandler(fi)
 
+def setup_honeybadgers(honeybadger_class, N):
+    sid = 'sidA'
+    # Generate threshold sig keys
+    sPK, sSKs = dealer(N, 2, seed=None)
+    # Generate threshold enc keys
+    ePK, eSKs = tpke.dealer(N, 2)
+
+    rnd = random.Random(None)
+    #print 'SEED:', seed
+    router_seed = rnd.random()
+    sends, recvs = simple_router(N, seed=router_seed)
+
+    badgers = [None] * N
+    threads = [None] * N
+    for i in range(N):
+        badgers[i] = HB(sid, i, 1, N, 1,
+                                    sPK, sSKs[i], ePK, eSKs[i],
+                                    sends[i], recvs[i])
+        threads[i] = gevent.spawn(badgers[i].run)
+
+def simple_router(N, maxdelay=0.005, seed=None):
+    """Builds a set of connected channels, with random delay
+
+    :return: (receives, sends)
+    """
+    rnd = random.Random(seed)
+    #if seed is not None: print 'ROUTER SEED: %f' % (seed,)
+
+    queues = [Queue() for _ in range(N)]
+    _threads = []
+
+    def makeSend(i):
+        def _send(j, o):
+            delay = rnd.random() * maxdelay
+            delay *= math.log(len(o)) * 7.5
+            gevent.spawn_later(delay, queues[j].put_nowait, (i,o))
+        return _send
+
+    def makeRecv(j):
+        def _recv():
+            (i,o) = queues[j].get()
+            #print 'RECV %8s [%2d -> %2d]' % (o[0], i, j)
+            return (i,o)
+        return _recv
+
+    return ([makeSend(i) for i in range(N)],
+            [makeRecv(j) for j in range(N)])
 
 
