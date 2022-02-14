8c802e47b1adde20c0b5522b3e4e2d4c5ea0dac2
1337samuels <giladsamuels1@gmail.com>
2022-02-14 14:14:48 +0000

    Run both tests

--- our_srcs/analyze_results.py
+++ our_srcs/analyze_results.py
@@ -1,11 +1,12 @@
 import sys
+import os
 from matplotlib import pyplot as plt
 import pandas
 import re
 
-RESULT_REGEX = "\(\d\d:\d\d:\d\d\) CRITICAL: Result: (?P<result_value>[\d]+.?[\d]*) \(params (?P<HB>[a-zA-Z]* [a-zA-Z]*), (?P<N>\d*), (?P<id>\d*), (?P<sz>\d*)\)"
+RESULT_REGEX = "\(\d\d:\d\d:\d\d\) CRITICAL: Result: bytes_sent=(?P<bytes_sent>\d+),total_time=(?P<total_time>[\d]+.?[\d]*) \(params (?P<HB>[a-zA-Z]* [a-zA-Z]*), (?P<N>\d*), (?P<id>\d*), (?P<sz>\d*)\)"
 
-SET_PARAMS = {"N": 10, "id": 6, "sz": 2**20}
+SET_PARAMS = {"N": 8, "id": 6, "sz": 2**16}
 PARAMS = ['N', 'id', 'sz']
 
 
@@ -16,8 +17,23 @@ def get_result_line(line):
 def update_dict_by_param(full_dict, result_dict, param):
     if result_dict['HB'] not in full_dict:
         full_dict[result_dict['HB']] = {}
-    full_dict[result_dict['HB']][result_dict[param]] = result_dict['result_value']
+    full_dict[result_dict['HB']][result_dict[param]] = result_dict['total_time']
 
+def get_lines(log_file):
+    all_lines = []
+    if os.path.isfile(log_file):
+        with open(log_file, 'r') as f:
+            all_lines += f.readlines()
+    elif os.path.isdir(log_file):
+        log_files = os.listdir(log_file)
+        for file_name in log_files:
+            cur_full_file = '\\'.join([log_file, file_name])
+            if os.path.isfile(cur_full_file):
+                with open(cur_full_file, 'r') as f:
+                    all_lines += f.readlines()
+    else:
+        assert False, f"what is {log_file}??"
+    return all_lines
 
 def extract_data(log_file):
     data_by_nodes = {}
@@ -25,8 +41,8 @@ def extract_data(log_file):
     data_by_size = {}
     dicts_by_param = {'N': data_by_nodes, 'id': data_by_id, 'sz': data_by_size}
 
-    with open(log_file, 'r') as f:
-        all_lines = f.readlines()
+    all_lines = get_lines(log_file)
+
     for l in all_lines:
         regex_match = re.match(RESULT_REGEX, l)
         if regex_match is None:
@@ -71,4 +87,5 @@ def main():
 
 
 main()
+
 
